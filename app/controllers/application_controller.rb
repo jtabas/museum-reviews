f42140f60d6ae3e1ab87644d068013abb8e2ac86
jd.macmurtrie <jamesd.macmurtrie@gmail.com>
2017-06-13 17:49:14 -0400

    devise changes round 4, hopefully last

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,8 +8,9 @@ class ApplicationController < ActionController::Base
   protected
 
  def configure_permitted_parameters
-   devise_parameter_sanitizer.permit( :sign_up, keys: [:first_name, :last_name,
-                                      :username, :password, :city,
-                                      :state, :email])
+   devise_parameter_sanitizer.permit(:sign_up, keys:
+                                    [:first_name, :last_name,
+                                     :username, :password, :city,
+                                     :state, :email])
  end
 end
--- app/controllers/museums_controller.rb
+++ app/controllers/museums_controller.rb
@@ -1,4 +1,3 @@
 class MuseumsController < ApplicationController
-  def index
-  end
+  def index; end
 end
