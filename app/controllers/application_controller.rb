356c9a3a1bb271c0622f7ff7d8501ec0b180c5c9
jd.macmurtrie <jamesd.macmurtrie@gmail.com>
2017-06-13 17:43:21 -0400

    devise changes round 4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
- # Prevent CSRF attacks by raising an exception.
- # For APIs, you may want to use :null_session instead.
+# Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?
@@ -8,8 +8,8 @@ class ApplicationController < ActionController::Base
   protected
 
  def configure_permitted_parameters
-   devise_parameter_sanitizer.permit(
-    :sign_up, keys: [:first_name, :last_name, :username, :password, :city,
-                     :state, :email])
+   devise_parameter_sanitizer.permit( :sign_up, keys: [:first_name, :last_name,
+                                      :username, :password, :city,
+                                      :state, :email])
  end
 end
