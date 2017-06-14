4656765ab95f1dace1adfff9c4f59508d16137fe
jd.macmurtrie <jamesd.macmurtrie@gmail.com>
2017-06-14 09:44:02 -0400

    last time, seriously

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-# Prevent CSRF attacks by raising an exception.
+  # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
@@ -8,9 +8,7 @@ class ApplicationController < ActionController::Base
   protected
 
  def configure_permitted_parameters
-   devise_parameter_sanitizer.permit(:sign_up, keys:
-                                    [:first_name, :last_name,
-                                     :username, :password, :city,
-                                     :state, :email])
+   devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name,
+                                     :username, :password, :city, :state, :email])
  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-        sessions: 'users/sessions'
-      }
+    sessions: 'users/sessions'
+  }
   resources :museums
-  root "museums#index"
+  root 'museums#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
