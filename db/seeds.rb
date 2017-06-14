1c614805eb9440cee216d8a351321a85b06560ad
ChrisDonhue0628 <chris.donohue0628@gmail.com>
2017-06-14 16:37:02 -0400

    I'm (not) done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,9 @@
 museum1 = Museum.create(name:'The Franklin Institute', description:'Sweet Sweet IMAX', hours:'9:30am to 5:00pm M,T,W,TH,F,S,SU', address:'222 N 20th St, Philadelphia, PA 19103', admission:20.0 )
 museum2 = Museum.create(name:'Philadelphia Museum of Art', description:'Come be a Snob!', hours:'9:30am to 5:00pm M,T,W,TH,F,S,SU', address:'2600 Benjamin Franklin Pkwy', admission:20.0)
 museums = [museum1, museum2]
+
 museums.each do |museum|
-  review1 = Review.create(rating: 3, body:"I would come back", museum: museum)
-  review2 = Review.create(rating: 1, body:"I would not come back", museum: museum)
-  review3 = Review.create(rating: 5)
+  Review.create(rating: 3, body:"I would come back", museum: museum)
+  Review.create(rating: 1, body:"I would not come back", museum: museum)
+  Review.create(rating: 5)
 end
