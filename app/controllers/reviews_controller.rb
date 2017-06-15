ee59dbe45afcf05798b1df870fe9b18fb94a644f
Brian Karol <Bskarol@gmail.com>
2017-06-15 15:10:22 -0400

    added create to reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,25 @@
 class ReviewsController < ApplicationController
+  def new
+    @review = Review.new
+    @museum = Museum.find(params[:museum_id])
+  end
+
+  def create
+    @museum = Museum.find(params[:museum_id])
+    @review = Review.create(review_params)
+    @review.museum = @museum
+    if @review.save
+      flash[:notice] = "Review added successfully"
+      redirect_to museum_path(@museum)
+    else
+      flash[:notice] = @review.errors.full_messages.to_sentence
+      render :new
+    end
+  end
+
+private
+
+  def review_params
+     params.require(:museum).permit(:rating, :body)
+  end
 end
