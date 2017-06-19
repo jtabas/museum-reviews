e43552d319f7791c9a3c0b79aad3f9fa902c15d6
ChrisDonhue0628 <chris.donohue0628@gmail.com>
2017-06-19 16:03:30 -0400

    Merging

--- app/models/museum.rb
+++ app/models/museum.rb
@@ -1,13 +1,9 @@
 # museum model, has many reviews and musuem categories
 class Museum < ApplicationRecord
   has_many :reviews
-<<<<<<< HEAD
   has_many :museum_categories
   has_many :categories, through: :museum_categories
   validates :name, uniqueness: true
-=======
-
   validates :name, presence: true
   validates :location, presence: true
->>>>>>> master
 end
--- spec/factories/reviews.rb
+++ spec/factories/reviews.rb
@@ -1,11 +1,8 @@
 FactoryGirl.define do
   factory :review do
-<<<<<<< HEAD
-=======
     rating 3
     body 'this museum was definitely museum-esque. if you want a museum, this is certainly that.'
     user
     museum
->>>>>>> master
   end
 end
