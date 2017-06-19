cf7c30bd522fd393be31ec6c04f29232cd4c7051
jd.macmurtrie <jamesd.macmurtrie@gmail.com>
2017-06-19 16:08:01 -0400

    votes mvc setuo

---
+++ app/controllers/votes_controller.rb
@@ -0,0 +1,7 @@
+class Api::V1::VotesController < ApplicationController
+  def create
+    review = Review.find(params[:review_id])
+    Vote.create(review: review, user: current_user, upvote: params[:vote])
+    render json: { rating: review.rating}
+  end
+end
