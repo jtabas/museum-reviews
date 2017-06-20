f81f8654a6aec9a1caf68493c044dc7e653b0c7f
ChrisDonhue0628 <chris.donohue0628@gmail.com>
2017-06-20 13:21:11 -0400

    Hound Config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -28,7 +28,6 @@ include AuthenticationHelper
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
-  config.include JsonHelper
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
