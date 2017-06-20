6d732345b8e45f76bfcd075a5c3772abf3d20f4d
tabs <jtabas@gmail.com>
2017-06-20 10:44:35 -0400

    votes mvc setup

---
+++ db/migrate/20170620143111_create_votes.rb
@@ -0,0 +1,11 @@
+class CreateVotes < ActiveRecord::Migration[5.1]
+  def change
+    create_table :votes do |t|
+      t.boolean :upvote, null: false
+      t.integer :review_id, null: false
+      t.integer :user_id, null: false
+      t.timestamps
+    end
+    add_index :votes, [:review_id, :user_id], unique: true
+  end
+end
