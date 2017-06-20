517400e2d51bc16f470509b9116cb66929dbffd4
tabs <jtabas@gmail.com>
2017-06-20 11:54:08 -0400

    more votes functionality

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,22 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions: 'users/sessions'
   }
+
   root 'museums#index'
-  namespace :admin do
-    resources :museums
-    resources :reviews
-  end
 
   resources :museums do
     resources :reviews
+  end
+
+  namespace :admin do
+    resources :users
+  end
+
+  namespace :api do
+    namespace :v1 do
+      resources :reviews, only: [] do
+        resources :votes, only: [:create]
+      end
+    end
   end
 end
