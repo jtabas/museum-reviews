a38e3486d72d556a53653d12a2fb93b29b02026f
tabs <jtabas@gmail.com>
2017-06-20 17:08:57 -0400

    fix codeship

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,6 +2,6 @@ class Api::V1::VotesController < ApplicationController
   def create
     review = Review.find(params[:review_id])
     Vote.create(review: review, user: current_user, upvote: params[:vote])
-    render json: { rating: review.rating}
+    render json: { rating: review.rating }
   end
 end
