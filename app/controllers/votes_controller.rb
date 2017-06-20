daa729093a1edbfc5ffe235cd23899003ff756d3
jd.macmurtrie <jamesd.macmurtrie@gmail.com>
2017-06-20 13:08:13 -0400

    controller bug fix, not votes

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,7 +1,7 @@
-class Api::V1::VotesController < ApplicationController
-  def create
-    review = Review.find(params[:review_id])
-    Vote.create(review: review, user: current_user, upvote: params[:vote])
-    render json: { rating: review.rating}
-  end
-end
+# class Api::V1::VotesController < ApplicationController
+#   def create
+#     review = Review.find(params[:review_id])
+#     Vote.create(review: review, user: current_user, upvote: params[:vote])
+#     render json: { rating: review.rating}
+#   end
+# end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
+  devise_for :users,
+   controllers: {
     sessions: 'users/sessions'
   }
   root 'museums#index'
