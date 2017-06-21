e7d6c5eb6eeb52771e0d4e0f410bf5ca384d8c81
jd.macmurtrie <jamesd.macmurtrie@gmail.com>
2017-06-21 10:02:54 -0400

    user crud actions and tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,16 +5,27 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
-
   end
 
   def update
     @user = User.find(params[:id])
-
+    if @user.update(user_params)
+      flash[:notice] = "Profile Updated Successfully"
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
   end
 
   def destroy
     @user = User.find(params[:id])
+    @user.destroy
+    redirect_to museums_path
+  end
 
+private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :username, :city, :state)
   end
 end
--- spec/features/user_actions/12_user_visits_profile_page_spec.rb
+++ spec/features/user_actions/12_user_visits_profile_page_spec.rb
@@ -1,14 +1,24 @@
 require 'rails_helper'
 
-feature 'Museums Show' do
-  let!(:museum) { FactoryGirl.create(:museum, name: "evnewevwunerwpunrpnu") }
+feature 'User visits user show page' do
+  let!(:user) { FactoryGirl.create(:user) }
 
-  scenario 'User visits Museum show page directly' do
-    visit museum_path(museum)
-    expect(page).to have_content museum.name
-    expect(page).to have_content museum.description
-    expect(page).to have_content museum.hours
-    expect(page).to have_content museum.address
-    expect(page).to have_content museum.admission
+  scenario 'User visits profile' do
+    sign_in_as(user)
+    visit root_path
+    click_link 'My Profile'
+
+    expect(page).to have_content user.first_name
+    expect(page).to have_content user.last_name
+    expect(page).to have_content user.email
+    expect(page).to have_content user.username
+    expect(page).to have_content user.city
+    expect(page).to have_content user.state
+  end
+
+  scenario 'User cannot visit profile if not signed in' do
+    visit root_path
+
+    expect(page).to_not have_link 'My Profile'
   end
 end
