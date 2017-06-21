d36a2808218f28c787ad00274b21f4af7312ae52
ChrisDonhue0628 <chris.donohue0628@gmail.com>
2017-06-21 10:01:47 -0400

    Code Climate  Fixes

--- react/src/containers/MuseumsContainer.js
+++ react/src/containers/MuseumsContainer.js
@@ -16,7 +16,7 @@ class MuseumsContainer extends Component {
   }
 
   getData () {
-    fetch('http://localhost:3000/api/v1/museums.json')
+    fetch('https://localhost:3000/api/v1/museums.json')
       .then(response => {
         if (response.ok) {
           return response;
