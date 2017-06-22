c46185e3ed2dcd8b49a3a1ee651c44efb775d226
ChrisDonhue0628 <chris.donohue0628@gmail.com>
2017-06-22 10:21:46 -0400

    Email Support

--- spec/features/email/recieve_email_for_reviewing_spec.rb
+++ spec/features/email/recieve_email_for_reviewing_spec.rb
@@ -1,15 +1,18 @@
-scenario "review a museum" do
-  museum = FactoryGirl.create(:museum)
-  user = FactoryGirl.create(:user)
+require 'rails_helper'
+feature 'User recieves an email' do
+  scenario "once they review a museum they get an email confirming that they did so" do
+    museum = FactoryGirl.create(:museum)
+    user = FactoryGirl.create(:user)
 
-  sign_in_as(user)
+    sign_in_as(user)
 
-  visit museum_path(museum)
+    visit museum_path(museum)
 
-  fill_in "Rating", with: "1"
-  fill_in "Review", with: "Total garbage."
-  click_button "Submit Review"
+    fill_in "Rating", with: "1"
+    fill_in "Review", with: "Total garbage."
+    click_button "Submit Review"
 
-  expect(page).to have_content("Total garbage.")
-  expect(ActionMailer::Base.deliveries.count).to eq(1)
+    expect(page).to have_content("Total garbage.")
+    expect(ActionMailer::Base.deliveries.count).to eq(1)
+  end
 end
--- spec/mailers/previews/review_mailer_preview.rb
+++
@@ -1,4 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/review_mailer
-class ReviewMailerPreview < ActionMailer::Preview
-
-end
