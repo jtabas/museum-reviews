c2c394815b23e7d6d1754ec8eb8729f96c722abb
jd.macmurtrie <jamesd.macmurtrie@gmail.com>
2017-06-22 18:46:56 -0400

    testing up

--- spec/features/votes/vote_spec.rb
+++ spec/features/votes/vote_spec.rb
@@ -1,14 +1,13 @@
 require 'rails_helper'
 
 xfeature "User votes" do
-  let!(:user) {FactoryGirl.create(:user) }
-  let!(:review) {FactoryGirl.create(:review, user: user, museum: museum) }
-  let!(:museum) {FactoryGirl.create(:museum) }
+  let!(:user) { FactoryGirl.create(:user) }
+  let!(:review) { FactoryGirl.create(:review, user: user, museum: museum) }
+  let!(:museum) { FactoryGirl.create(:museum) }
   it 'user upvotes another review' do
     sign_in_as(user)
     visit museum_path(museum)
     click_button 'Downvote'
-    save_and_open_page
 
     expect(page).to have_content('Score: -1')
   end
