b8fc0ee570d03ffc00f2823044b53d5618bd963e
Brian Karol <Bskarol@gmail.com>
2017-06-22 19:35:58 -0400

    updated changes

--- react/src/components/SearchBar.js
+++ react/src/components/SearchBar.js
@@ -2,12 +2,6 @@ import React from 'react';
 const SearchBar = props => {
   return (
     <div className="search-bar small-12 columns" >
-<<<<<<< HEAD
-      <h4>
-        Search for a Museum:
-      </h4>
-=======
->>>>>>> master
       <input
         type='text'
         placeholder="Search for a Museum"
