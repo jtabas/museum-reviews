50f56af3cba104cd9e5f6b6d81cc4051b469241f
tabs <jtabas@gmail.com>
2017-06-22 18:41:06 -0400

    Changes

--- app/controllers/museums_controller.rb
+++ app/controllers/museums_controller.rb
@@ -7,6 +7,7 @@ class MuseumsController < ApplicationController
     @museum = Museum.find(params[:id])
     @reviews = @museum.reviews
     @review = Review.new
+    @hours = @museum.hours.split('\\n')
 
     @rating = if (@museum.rating % 1).zero? then @museum.rating.to_i else @museum.rating.round(1) end
   end
