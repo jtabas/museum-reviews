74bd0b464e76d20edfa319b8561f8a04ed70b4a3
tabs <jtabas@gmail.com>
2017-06-22 14:13:55 -0400

    Votes are working!

--- app/controllers/api/v1/votes_controller.rb
+++ app/controllers/api/v1/votes_controller.rb
@@ -10,7 +10,7 @@ class Api::V1::VotesController < ApplicationController
     else
       vote = Vote.create(review: review, user: current_user, upvote: params[:vote])
     end
-    # VoteMailer.new_vote(vote).deliver_now
+    VoteMailer.new_vote(vote).deliver_now
     render json: { score: review.score }
   end
 end
