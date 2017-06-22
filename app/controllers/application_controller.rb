d9e594448450a59706086c181c996f5d09bb8060
jd.macmurtrie <jamesd.macmurtrie@gmail.com>
2017-06-22 15:18:14 -0400

    brackets?!?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,20 +7,20 @@ class ApplicationController < ActionController::Base
 
   protected
 
- def configure_permitted_parameters
-   devise_parameter_sanitizer.permit(devise_parameter_sanitizer.permit(
-     :sign_up,
-     keys: [
-       :first_name,
-       :last_name,
-       :username,
-       :password,
-       :city,
-       :state,
-       :email,
-       :avatar
-          ]
-        )
-      )
- end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(devise_parameter_sanitizer.permit(
+    :sign_up,
+    keys: [
+      :first_name,
+      :last_name,
+      :username,
+      :password,
+      :city,
+      :state,
+      :email,
+      :avatar
+    ]
+    )
+    )
+  end
 end
